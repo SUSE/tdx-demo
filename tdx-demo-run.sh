945638079ee184df27127415172857676c9c2691
Claudio Fontana <cfontana@suse.de>
2023-12-15 09:40:32 +0000

    tdx-demo-run: rename tdx to tdx0
    looks better.
    
    Reported-by: Nikolay Borisov <nborisov@suse.com>
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- tdx-demo-run.sh
+++ tdx-demo-run.sh
@@ -2,9 +2,9 @@ FILE=~/tdx-guest.qcow2
 
 /usr/bin/qemu-system-x86_64 \
         -accel kvm \
-        -object tdx-guest,sept-ve-disable=on,id=tdx \
+        -object tdx-guest,sept-ve-disable=on,id=tdx0 \
         -object memory-backend-memfd-private,id=ram1,size=4G \
-        -machine q35,kernel_irqchip=split,confidential-guest-support=tdx,memory-backend=ram1 \
+        -machine q35,kernel_irqchip=split,confidential-guest-support=tdx0,memory-backend=ram1 \
         -cpu host,pmu=off,-kvm-steal-time \
         -smp 4 \
         -drive file=${FILE},if=virtio \
